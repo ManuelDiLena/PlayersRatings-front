030769d05d69a033dfc9c4f8c9d01148d57f4b87
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-09-29 11:53:41 +0200

    Communication with the back

--- src/services/players.js
+++ src/services/players.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/players'
+const baseUrl = 'http://localhost:3001/api/players'
 
 const getPlayers = () => {
     const request = axios.get(baseUrl)
